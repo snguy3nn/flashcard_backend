41f87ad544b4863831af785a38710afe2509f3a6
Stephen Nguyen <snguy3nn@gmail.com>
2021-07-05 14:32:45 -0500

    Created get and post functionality for the collection and flashcard

--- flashcard_project/flashcard/views.py
+++ flashcard_project/flashcard/views.py
@@ -1,11 +1,9 @@
-from django.http import Http404
-from .models import Flashcard
-from .models import Collection
-from .serailizers import CollectionSerializer
-from .serailizers import FlashcardSerializer
+from .models import Collection, Flashcard
+from .serailizers import CollectionSerializer, FlashcardSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import Http404
 
 
 class CollectionList(APIView):
@@ -17,6 +15,48 @@ class CollectionList(APIView):
 
     def post(self, request):
         serializer = CollectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CollectionDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Collection.objects.get(pk=pk)
+        except Collection.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        collection = self.get_object(pk)
+        serializer = FlashcardSerializer(collection)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        collection = self.get_object(pk)
+        serializer = CollectionSerializer(collection, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        collection = self.get_object(pk)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class FlashcardList(APIView):
+
+    def get(self, request):
+        flashcard = Flashcard.objects.all()
+        serializer = FlashcardSerializer(flashcard, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FlashcardSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
